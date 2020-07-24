36ba071e53436811ca03dea29d42677edc606be6
Omri Hopson <omri.hopson@gmail.com>
2020-07-24 16:27:37 -0400

    ui frame added

--- app/src/main/java/com/assignment/yolitodo/MainActivity.java
+++ app/src/main/java/com/assignment/yolitodo/MainActivity.java
@@ -2,13 +2,31 @@ package com.assignment.yolitodo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+
+    /** Called when the user taps the Send button */
+    public void addList(View view) {
+        // Do something in response to button
+
+        Context context = getApplicationContext();
+        CharSequence text = "Aite then so now we have toasts working :D";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
