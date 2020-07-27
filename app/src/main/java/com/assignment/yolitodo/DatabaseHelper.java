70a933460c67185500e8ce8ce518a1bea9f60cad
Omri Hopson <omri.hopson@gmail.com>
2020-07-26 23:44:32 -0400

    added database

--- app/src/main/java/com/assignment/yolitodo/DatabaseHelper.java
+++ app/src/main/java/com/assignment/yolitodo/DatabaseHelper.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
-    public static final int DATABASE_VERSION = 1;
+    public static final int DATABASE_VERSION = 2;
     public static final String DATABASE_NAME = "ToDoList.db";
     public static final String TABLE_NAME = "parents";
 
